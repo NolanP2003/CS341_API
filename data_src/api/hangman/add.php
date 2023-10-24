4a2d5af1e66d1231b46aab247c66bad9cbc841f8
IsabelM8 <isabelmattivi.05@gmail.com>
2023-10-24 10:42:23 -0400

    Creating option to add word for hangman

--- data_src/api/hangman/add.php
+++ data_src/api/hangman/add.php
@@ -9,18 +9,31 @@ if ($connection->connect_error) {
     die("Connection failed: ".$connection->connect_error);
 }
 
-// We're doing it the hard way I guess
-$sql = $connection->prepare("INSERT INTO hangman (word) VALUES (UPPER(?));");
-$sql->bind_param("s", $word); // No SQLi allowed
-
 $word = $_POST["word"]; // Receive the Word of User
-$sql->execute();
 
-$message = ["message" => "Added a word successfully", "word" => $word]; // It works
-// Should change it in case it doesn't work
+$sql = $connection->prepare("SELECT word FROM hangman WHERE word = UPPER(?);");
+$sql->bind_param("s", $word);
+$sql->execute();
+$data = $sql->get_result();
+
+if ($data->num_rows > 0) { // If there are more than 0 rows
+    $message = ["message" => "Rejected. Word is already in Database", "word" => $word];
+    // $response = "rejected";
+} else { // If the word isn't in the database
+    // We're doing it the hard way I guess
+    $sql = $connection->prepare("INSERT INTO hangman (word) VALUES (UPPER(?));");
+    $sql->bind_param("s", $word); // No SQLi allowed
+    $sql->execute();
+
+    $message = ["message" => "Added a word successfully", "word" => $word];
+    // $response = "success";
+}
+
 echo json_encode($message);
 
 $sql->close();
 // Close the database connection
 $connection->close();
+
+// echo "<script>document.getElementById('response').value='".$response."';</script>";
 ?>
