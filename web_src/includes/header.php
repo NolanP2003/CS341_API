e68404fb348d20c8571dce4fd0bfc969ce1c0451
danistrausburger <dannysue2020@gmail.com>
2023-10-26 08:10:43 -0400

    creating header file

--- web_src/includes/header.php
+++ web_src/includes/header.php
@@ -1,1 +1,43 @@
-<!-- Yay! Another placeholder! -->
+<html>
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar bg-blue">
+            <a class="navbar-brand" href="https://etown.edu/">
+                <img id="logo" src="../includes/images/logo.png" alt="Logo" width='100px.'>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../general/home.php">
+                            <i class="fas fa-home"></i> Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="settings.php">
+                            <i class="fas fa-cog"></i> Settings
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">
+                            <i class="fas fa-info-circle"></i> About
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../games/menu.php">
+                            <i class="fas fa-gamepad"></i> Games
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    </html>
