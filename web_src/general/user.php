709912d94581076e229b7943de5b501303379377
IsabelM8 <isabelmattivi.05@gmail.com>
2023-11-01 12:55:01 -0400

    Create user.php

---
+++ web_src/general/user.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>User</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar bg-blue">
+            <a class="navbar-brand" href="https://etown.edu/">
+                <img id="logo" src="../includes/images/logo.png" alt="Logo" width='100px.'>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">
+                            <i class="fas fa-home"></i> Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="settings.php">
+                            <i class="fas fa-cog"></i> Settings
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">
+                            <i class="fas fa-info-circle"></i> About
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../games/menu.php">
+                            <i class="fas fa-gamepad"></i> Games
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section id="user-sec">
+            <br>
+            <div id="text">
+                <h2 style="text-align:center">User</h2>
+            </div> 
+            <br>
+            <form action="" method="post" id="add-user" style="text-align: center">
+            Enter your username: <input type="text" name="user">
+            <input type="submit" value="Submit">
+            </form>
+        </section>
+    </main>
+</body>
+</html>
