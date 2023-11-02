604ebdbdbaecd77ea618a12d1a40e58725deb64c
danistrausburger <dannysue2020@gmail.com>
2023-11-02 10:11:49 -0400

    adding login page and linking to it

--- web_src/general/login.php
+++ web_src/general/login.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>User</title>
+    <title>Login</title>
     <link rel="stylesheet" type="text/css" href="../stylesheets/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
-
 <body>
-    <header>
+<header>
         <nav class="navbar navbar-expand-lg navbar bg-blue">
-            <a class="navbar-brand" href="https://etown.edu/">
+            <a class="navbar-brand" href="index.php">
                 <img id="logo" src="../includes/images/logo.png" alt="Logo" width='100px.'>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,24 +36,30 @@
                             <i class="fas fa-gamepad"></i> Games
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">
+                            <i class="fas fa-key"></i> Login
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
     </header>
+    <br>
+    <section id="login">
+        <div id="welcome-text">Login</div>
+        <br><br>
+        <form action="" method="post">
+            <div class="container">
+                <label for="username"><b>Username: </b></label>
+                <input type="text" placeholder="Enter Username" name="username" required>
 
-    <main>
-        <section id="user-sec">
-            <br>
-            <div id="text">
-                <h2 style="text-align:center">User</h2>
-            </div> 
-            <br>
-            <form action="../../data_src/api/user/add.php" method="post" id="add-user" style="text-align: center">
-            Enter your username: <input type="text" name="user">
-            Enter your email: <input type="text" name="email">
-                <input type="submit" value="Submit">
-            </form>
-        </section>
-    </main>
+                <label for="password"><b>Password: </b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+
+                <button type="submit">Login</label>
+            </div>
+        </form>
+    </section>
 </body>
 </html>
--- web_src/general/settings.php
+++ web_src/general/settings.php
@@ -92,6 +92,11 @@
                             <i class="fas fa-gamepad"></i> Games
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">
+                            <i class="fas fa-key"></i> Login
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
