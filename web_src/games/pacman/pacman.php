231ab9bffbc6d65f43ccf37e6830154f1baa26f6
NolanP2003 <pettitn@etown.edu>
2023-11-12 14:40:09 -0500

    adding pacman stuff

--- web_src/games/pacman/pacman.php
+++ web_src/games/pacman/pacman.php
@@ -39,6 +39,10 @@
             </ul>
         </div>
     </nav>
+<main>
+    
+    
+    <h1 id="Welcome">Welcome to Pac-Man! Click the screen to start the game.</h1>
 <div class="pacman-container">
     <svg id="pacmanSVG"
          version="1.1"
@@ -49,10 +53,13 @@
     </svg>
     <script src="pacman.js"></script>
     <div>
-    <p>Content provided by Johan Hasselqvist and Lars Gunther.</p>
-    <p>Source: <a href="https://github.com/Hasselmannen/pacman/blob/master/README.markdown">Johan Hasselqvist's GitHub</a></p>
+        <p>Content provided by Johan Hasselqvist and Lars Gunther.</p>
+        <p>Source: <a href="https://github.com/Hasselmannen/pacman/blob/master/README.markdown">Johan Hasselqvist's GitHub</a></p>
     </div>
 </div>
-
+<aside id="win-container" class="hidden">
+    <p>hello</p>
+</aside>
+</main>
 </body>
 </html>
