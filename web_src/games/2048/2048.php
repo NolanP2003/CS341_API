c1d8f96ad2f5692e780c1dfaeff2ecc8689eb665
NolanP2003 <pettitn@etown.edu>
2023-11-16 08:11:50 -0500

    stylesheet for 2048

--- web_src/games/2048/2048.php
+++ web_src/games/2048/2048.php
@@ -3,9 +3,44 @@
 <head>
   <meta charset="UTF-8">
   <title>2048 Game</title>
-  <link rel="stylesheet" href="2048.css">
+  <link rel="stylesheet" href="../../stylesheets/2048.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar bg-blue">
+        <a class="navbar-brand" href="index.php">
+            <img id="logo" src="../../includes/images/logo.png" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../../general/index.php">
+                        <i class="fas fa-home"></i> Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../general/settings.php">
+                        <i class="fas fa-cog"></i> Settings
+                    </a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="../../general/about.php">
+                            <i class="fas fa-info-circle"></i> About
+                        </a>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../menu.php">
+                        <i class="fas fa-gamepad"></i> Games
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
   <div class="container">
     <div class="game-board">
       <!-- The game board will be generated dynamically using JavaScript -->
