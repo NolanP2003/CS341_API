e61c77b6cfd82bacc7155a0acb9f05eecde2eb4f
Stephaniemotz <smotz0328@gmail.com>
2023-11-20 23:14:21 -0500

    formatting sub nav bar

--- web_src/general/settings.php
+++ web_src/general/settings.php
@@ -1,36 +1,39 @@
 <?php
 session_start();
-if (!isset($_SESSION['id'])) {
-    header('Location:login.php');
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>My Website</title>
+    <title>Trivia Settings</title>
     <link rel="stylesheet" type="text/css" href="../stylesheets/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
     <script>
         // This is for form submissions
-        document.addEventListener("DOMContentLoaded", function() { // Runs when doc loads
-            const form = document.getElementById("add-word"); // Reference to HTML element add-word
-            const message = document.getElementById("response"); // Reference to div
-            const input = form.querySelector("input[name='word']"); // Reference to input element where user enters word
+        document.addEventListener("DOMContentLoaded", function() {
+            const form = document.getElementById("add-question");
+            const message = document.getElementById("response");
+            const input = form.querySelector("input[name='question']");
+            const answerInput1 = form.querySelector("input[name='answer1']");
+            const answerInput2 = form.querySelector("input[name='answer2']");
+            const answerInput3 = form.querySelector("input[name='answer3']");
 
-            form.addEventListener("submit", function(e) { // Listens for submit button
-                e.preventDefault(); // Prevents page from reloading to add.php?
-                const formData = new FormData(form); // Access to the form data, to send AJAX request
+            updateQuestionList();
+
+            form.addEventListener("submit", function(e) {
+                e.preventDefault();
+                const formData = new FormData(form);
 
                
-                fetch(form.getAttribute("action"), { // Makes HTTP request
-                    method: "POST", body: formData // Sends POST request with form data
-                }).then(response => response.json()) // then expects response in JSON format
+                fetch(form.getAttribute("action"), {
+                    method: "POST", body: formData
+                }).then(response => response.json())
                 .then(data => {message.textContent = data.message; message2.textContent = ''; input.value = ''; // Sets message and clears text field
+                    answerInput1.value = ''; answerInput2.value = ''; answerInput3.value = '';
                     message.style.color = data.status === "success" ? "green" : "red"; // Changes color if successful
-                    updateWordList();
+                    updateQuestionList();
                     console.log("here");
                 }).catch(error => { // Error
                     
@@ -42,9 +45,9 @@ if (!isset($_SESSION['id'])) {
                 });
             });
 
-            const form2 = document.getElementById("delete-word");
+            const form2 = document.getElementById("delete-question");
             const message2 = document.getElementById("response2");
-            const input2 = form2.querySelector("input[name='word']");
+            const input2 = form2.querySelector("input[name='questionID']");
 
             form2.addEventListener("submit", function(e) {
                 e.preventDefault();
@@ -55,29 +58,22 @@ if (!isset($_SESSION['id'])) {
                 }).then(response => response.json())
                 .then(data => {message2.textContent = data.message; message.textContent = ''; input2.value = ''; // data.<message> comes from php
                     message2.style.color = data.status === "success" ? "green" : "red"; // data.<status> comes from php
-                    updateWordList();
+                    updateQuestionList();
                 }). catch(error => {
                     message2.textContext = "An error occurred.";
                     message2.style.color = "red";
                 });
             });
-            function updateWordList() {
-                const wordList = document.getElementById("word-list");
-                fetch('http://localhost/CS341_API/data_src/api/hangman/list.php', {method: 'get'}) // TODO: Change file path for FTP
+            function updateQuestionList() {
+                const wordList = document.getElementById("question-list");
+                fetch('http://localhost/CS341_API/data_src/api/trivia/read.php', {method: 'get'}) // TODO: Change file path for FTP
                     .then(response => response.json())
                     .then(data => {wordList.innerHTML = data.join('<br>');}).catch(console.error);
             }
-            // updateWordList();
-
+            updateQuestionList();
+            
         });
-
-        function updateWordList() {
-                const wordList = document.getElementById("word-list");
-                fetch('http://localhost/CS341_API/data_src/api/hangman/list.php', {method: 'get'}) // TODO: Change file path for FTP
-                    .then(response => response.json())
-                    .then(data => {wordList.innerHTML = data.join('<br>');}).catch(console.error);
-        }
-
+       
     </script>
 
 </head>
@@ -97,10 +93,15 @@ if (!isset($_SESSION['id'])) {
                         <a class="nav-link" href="index.php">
                             <i class="fas fa-home"></i> Home
                         </a>
-                    </li>
-                    <li class="nav-item">
+                        <li class="nav-item">
+                        <div class="subnav">
                         <a class="nav-link" href="settings.php">
-                            <i class="fas fa-cog"></i> Settings
+                        <i class="fas fa-cog"></i>Settings<button class="subnavbtn"></button>
+                            <div class="subnav-content">
+                                <a href="triviaSettings.php">Trivia Settings </a>
+                                <a href="hangmanSettings.php">Hangman Settings </a>
+                            </div>
+                        </div>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -113,50 +114,26 @@ if (!isset($_SESSION['id'])) {
                             <i class="fas fa-gamepad"></i> Games
                         </a>
                     </li>
+                    <!-- Checking whether to display login or logout button. -->
+                    <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
                     <li class="nav-item">
                         <a class="nav-link" href="logout.php">
                             <i class="fas fa-key"></i> Logout
                         </a>
                     </li>
+                    <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">
+                            <i class="fas fa-key"></i> Login
+                        </a>
+                    </li>
+                    <?php } ?>
                 </ul>
             </div>
         </nav>
     </header>
 
-    <section id="sub-settings">
-        <ul class="sub-nav">
-            <div class="sub-nav-items">
-                <li><a href="settings.php">Hangman</a></li>
-                <li><a href="trivia.php">Trivia</a></li>
-            </div>
-        </ul>
-    </section>
-
     <main>
-        <section id="game">
+        <section id="settingsHome">
             <br>
-            <div id="welcome-text">Hangman Settings</div>
-            <form action="../../data_src/api/hangman/add.php" method="post" id="add-word">
-            &nbspAdd A Word: <input type="text" name="word"><br>
-            &nbsp<input type="submit" value="Submit">
-                <div id="response"></div>
-            </form>
-            <br>
-            <form action="../../data_src/api/hangman/delete.php" method="post" id="delete-word">
-            &nbspDelete A Word: <input type="text" name="word"><br>
-            &nbsp<input type="submit" value="Submit">
-                <div id="response2"></div>
-            </form>
-            <br>
-            <div id="word-list">
-                <button type="button" onclick="updateWordList()">Show word list</button>
-            </div>
-            <br>
-            <div id="hide-button"> 
-                <button onClick="window.location.reload();">Hide word list</button>
-            </div>
-
-        </section>
-    </main>
-</body>
-</html>
+            <div id="welcome-text">Welcome to our Main Settings Page!</div>
