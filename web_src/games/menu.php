b9fc901f794ed0559786ea8a334085e31c6295b8
Stephaniemotz <smotz0328@gmail.com>
2023-12-02 00:27:46 -0500

    Style changes for game menu

--- web_src/games/menu.php
+++ web_src/games/menu.php
@@ -13,6 +13,7 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>Menu for game page</title>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar bg-blue">
             <a class="navbar-brand" href="index.php">
@@ -71,9 +72,20 @@ session_start();
             </div>
         </nav>
 
+    <div class="path">
+        <div class="pacman">
+            <div class="pacman_eye"></div>
+            <div class="pacman_mouth"></div>
+        </div>
+        <div class="ghost">
+        <div class="eyes"></div>
+        <div class="skirt"></div>
+        </div>
+    </div>
 
+    <br><br>
     <div id="welcome-message">
-        <p>Welcome to our game selection page!</p>
+        <p>Game Selection <br> Page!</p>
     </div>
 
     <div id="game-select">
@@ -86,6 +98,124 @@ session_start();
             <option value="2048">2048 - Etown Edition</option>
         </select>
     </div>
+    <br><br><br><br><br><br><br>
+
+    <div class="main-container">
+        <div class="mario-block" onclick="showChocolate()">
+            <input type="checkbox" id="1">
+            <div class= "gif-container"></div>
+            <label for="1">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </label>
+        </div>
+        <div class="mario-block" onclick="showTruman()">
+            <div class= "gif-container"></div>
+            <input type="checkbox" id="2">
+            <label for="2">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </label>
+        </div>
+        <div class="mario-block mario-block--question" onclick="showBlueJay()">
+            <div class= "gif-container"></div>
+            <input type="checkbox" id="3">
+            <label for="3">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="question-mark"></div>
+            </label>
+        </div>
+        <div class="mario-block" onclick="showCarrotCake()">
+            <div class= "gif-container"></div>
+            <input type="checkbox" id="4">
+            <label for="4">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </label>
+        </div>
+    </div>
+
+    
+    <script>
+        function showBlueJay() {
+        const bluejayImage = new Image();
+
+        bluejayImage.src = "menuImages/bluejayLogo.png"; 
+
+        // Adding style to bluejay
+        bluejayImage.style.width = '5%';
+        bluejayImage.style.height = '9%'; 
+        bluejayImage.style.position = 'absolute';
+        bluejayImage.style.top = '84%'; 
+        bluejayImage.style.left = '53%'; 
+        bluejayImage.style.transform = 'translate(-50%, -50%)'; 
+
+        document.body.appendChild(bluejayImage);
+        }
+    </script>
+
+    <script>
+        function showCarrotCake() {
+        const carrotCakeImage = new Image();
+
+        carrotCakeImage.src = "menuImages/carrotCake.png"; 
+
+        // Adding style to Carrot Cake (yummy)
+        carrotCakeImage.style.width = '6%';
+        carrotCakeImage.style.height = '11%'; 
+        carrotCakeImage.style.position = 'absolute';
+        carrotCakeImage.style.top = '85%'; 
+        carrotCakeImage.style.left = '58.5%'; 
+        carrotCakeImage.style.transform = 'translate(-50%, -50%)'; 
+
+        document.body.appendChild(carrotCakeImage);
+        }
+    </script>
+
+    <script>
+        function showTruman() {
+        const trumanImage = new Image();
+
+        trumanImage.src = "menuImages/truman.png"; 
+
+        // Adding style to Truman 
+        trumanImage.style.width = '6%';
+        trumanImage.style.height = '9%'; 
+        trumanImage.style.position = 'absolute';
+        trumanImage.style.top = '85%'; 
+        trumanImage.style.left = '47.5%'; 
+        trumanImage.style.transform = 'translate(-50%, -50%)'; 
+
+        document.body.appendChild(trumanImage);
+        }
+    </script>
+
+    <script>
+        function showChocolate() {
+        const chocolateImage = new Image();
+
+        chocolateImage.src = "menuImages/chocolate.png"; 
+
+        // Adding style to Chocolate 
+        chocolateImage.style.width = '5%';
+        chocolateImage.style.height = '10%'; 
+        chocolateImage.style.position = 'absolute';
+        chocolateImage.style.top = '84%'; 
+        chocolateImage.style.left = '42%'; 
+        chocolateImage.style.transform = 'translate(-50%, -50%)'; 
+
+        document.body.appendChild(chocolateImage);
+        }
+    </script>
 
     <script>
         function loadGame(select) {
@@ -95,5 +225,8 @@ session_start();
             }
         }
     </script>
+
+        
+
 </body>
 </html>
